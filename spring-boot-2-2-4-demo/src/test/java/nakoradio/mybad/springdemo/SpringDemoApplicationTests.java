b4fe80c5b8a5d70bb7bcaeaf3da12353f228765d
Arch-vile <mikko.ravimo@gmail.com>
2021-04-20 15:57:59 +0300

    TEMP

--- spring-boot-2-2-4-demo/src/test/java/nakoradio/mybad/springdemo/SpringDemoApplicationTests.java
+++ spring-boot-2-2-4-demo/src/test/java/nakoradio/mybad/springdemo/SpringDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package nakoradio.mybad.springdemo;
 
+import static io.restassured.RestAssured.when;
+
+import nakoradio.mybad.springdemo.SpringDemoApplication.SimpleForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class SpringDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		SimpleForm formData = new SimpleForm();
+		formData.name = "too long name";
+
+		when().post("/form", formData)
+				.then()
+				.statusCode(HttpStatus.BAD_REQUEST.value());
+
+
 
 
 
